ca988b284e4954c8e7c1599767ddc3c52228b32e
Dibash Thapa <dibashthapa55@gmail.com>
2024-09-14 14:58:17 +0545

    Initial Commit

---
+++ Nepali Calendar/Nepali_CalendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  Nepali_CalendarApp.swift
+//  Nepali Calendar
+//
+//  Created by mac on 14/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct Nepali_CalendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
